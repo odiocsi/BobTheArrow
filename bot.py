e2f6a13a3c3a2bb52c3840df32ee248fda2bfb87
odiocsi <odiocsi@gmail.com>
2025-01-26 03:34:45 +0100

    added music.py and some commands

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import os
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+import music
+
+## Config
 
 intents = discord.Intents.default()
 intents.messages = True 
@@ -9,26 +12,44 @@ intents.message_content = True
 bot = commands.Bot(command_prefix=".", intents=intents)
 allowed_channel_id = None
 
+mdown = music.MusicDownloader()
+
 @bot.event
 async def on_ready():
     print('A bot elindult.')
 
+## Parancsok
 @bot.command()
 async def test(ctx):
     await ctx.send("Hello World!")
 
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("Először lépj be egy hangcsatornába!")
 
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("Nem vagyok egy hangcsatornában sem!")
 
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def set_channel(ctx, channel: discord.TextChannel):
+    """Restricts bot activity to a specific channel."""
     global allowed_channel_id
-    allowed_channel_id = channel.id  # Store the channel ID
+    allowed_channel_id = channel.id 
     await ctx.send(f"A bot mostantól csak a következő csatornát látja: {channel.mention}")
 
 @bot.command()
 @commands.has_permissions(administrator=True)
 async def clear_channel(ctx):
+    """Clears channel restrictions."""
     global allowed_channel_id
     allowed_channel_id = None
     await ctx.send("A bot mostantól látja az összes csatornát.")
@@ -45,7 +66,7 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-
+## Bot init
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 if TOKEN is None:
@@ -53,5 +74,5 @@ if TOKEN is None:
     
 try:
     bot.run(TOKEN)
-except:
-    raise ValueError("Hibás token.")
+except Exception as e:
+    print(f"Hiba történt: {e}")
---
+++ music.py
@@ -0,0 +1,44 @@
+from yt_dlp import YoutubeDL
+
+## Config
+
+download_folder = "music"
+
+search_opts = {
+    'format': 'bestaudio/best',  
+    'noplaylist': True,  
+    'quiet': True,               
+    'extract_flat': True,      
+    'default_search': 'ytsearch',  
+}
+
+download_opts = {
+    'format': 'bestaudio/best',  
+    'outtmpl': f'{download_folder}/%(title)s.%(ext)s', 
+    'noplaylist': True,  
+}
+
+## Downloader class
+
+class MusicDownloader:
+    def __init__(self):
+        self.playlist = []
+        self.results = []
+
+    def search_youtube(self, query, max_results=5):
+        self.results = []
+        with YoutubeDL(search_opts) as ydl:
+            search_result = ydl.extract_info(f'ytsearch{max_results}:{query}', download=False)
+        return search_result
+    
+    def download(self, url):
+        """Downloads a YouTube audio file and returns its file path."""
+        with YoutubeDL(download_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            file_path = ydl.prepare_filename(info)  
+            return file_path  
+
+
+
+#for id, entry in enumerate(search_result['entries'], start=1):
+#    print(f"{id}. {entry['title']} ({entry['url']})")
