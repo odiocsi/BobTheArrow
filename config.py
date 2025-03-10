794f5382973b1d796cdd557bd358c3260f7c0c17
odiocsi <odiocsi@gmail.com>
2025-03-10 04:46:29 +0100

    implemented module based command configuration

--- config.py
+++ config.py
@@ -2,3 +2,12 @@ default_lang = "hu"
 default_command_prefix = "."
 download_path = "music"
 database_path = 'json/database.json'
+
+musicplayer = True
+rivalsapi = True
+lolapi = False
+moderation = True
+welcome = True
+clear = True
+prefixchange = True
+systemmessage = True
