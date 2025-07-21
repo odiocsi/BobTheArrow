8bcd5ba505fbfc0531b4b6940711a48819001cb1
odiocsi <odiocsi@gmail.com>
2025-07-21 18:27:58 +0200

    changed config according to the changes, added the new marvel rivals season

--- config.py
+++ config.py
@@ -3,15 +3,18 @@
 default_lang = "en" #hu/en
 default_command_prefix = "."
 
-#Folders
-download_path = "music/"
 database_path = 'json/database.json'
+
+#Misc
+activity_status = "Infinity>Flame"
 
 #Components
 setlang = True
 musicplayer = True
 rivalsapi = True
-lolapi = True #not yet implemented
+lolapi = True
+membercount = True
+serverstats = True
 moderation = True
 welcome = True
 clear = True
@@ -19,4 +22,4 @@ prefixchange = True
 systemmessage = True
 
 #RivalsAPI
-seasons = ["0", "1", "1.5", "2", "2.5"] #the last is always the default
+seasons = ["0", "1", "1.5", "2", "2.5", "3"] #the last is always the default
